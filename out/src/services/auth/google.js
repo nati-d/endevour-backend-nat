0d24bb42e126ffebe64d86b84a66e790b5813810
AbelMaireg <abelrighthere@gmail.com>
2024-04-20 22:47:55 +0300

    google sign in

---
+++ out/src/services/auth/google.js
@@ -0,0 +1,46 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const passport_1 = __importDefault(require("passport"));
+const index_1 = __importDefault(require("../../prisma/index"));
+var GoogleStrategy = require('passport-google-oauth2').Strategy;
+passport_1.default.use(new GoogleStrategy({
+    clientID: "297273504938-t8cdvjalf06v6229t9e0g2ds2a0k3549.apps.googleusercontent.com",
+    clientSecret: "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",
+    callbackURL: 'http://localhost:3000/auth/google/callback',
+    scope: ['profile', 'email']
+}, async function verify(accessToken, refreshToken, profile, cb) {
+    let user = await index_1.default.client.user_credential.findFirst({
+        where: {
+            credential_id: profile.id,
+            provider: profile.provider,
+        }
+    });
+    if (!user) {
+        let newUser = await index_1.default.client.user.create({
+            data: {
+                first_name: profile.given_name,
+                last_name: profile.family_name,
+                email: profile.email,
+                profile_image: profile.picture,
+            }
+        });
+        if (newUser.id)
+            await index_1.default.client.user_credential.create({
+                data: {
+                    user_id: newUser.id,
+                    credential_id: profile.id,
+                    provider: profile.provider
+                }
+            });
+    }
+    return cb(null, profile);
+}));
+passport_1.default.serializeUser((user, done) => {
+    done(null, user);
+});
+passport_1.default.deserializeUser((user, done) => {
+    done(null, user);
+});
