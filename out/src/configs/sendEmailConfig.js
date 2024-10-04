323944082c3e77a8584af12d15ab0c075f211be8
nati-d <nathnaeldes@gmail.com>
2024-10-04 11:47:58 +0300

    build updated email

--- out/src/configs/sendEmailConfig.js
+++ out/src/configs/sendEmailConfig.js
@@ -6,16 +6,16 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const nodemailer_1 = __importDefault(require("nodemailer"));
 const sendEmailConfig = (sendTo, subject, html, file) => {
     const transporter = nodemailer_1.default.createTransport({
-        host: "api.endevour.org",
+        host: "api.devidends.org",
         port: 465,
         secure: true,
         auth: {
-            user: "test@api.endevour.org",
-            pass: "Z2Q;@}HCl(Rx",
+            user: "opportunities@devidends.org",
+            pass: "NewYear#2025?",
         },
     });
     return transporter.sendMail({
-        from: "test@api.endevour.org",
+        from: "opportunities@devidends.org",
         to: sendTo,
         subject: subject,
         html: html,
